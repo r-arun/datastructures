351e459d9debab8338fde7d21a83c4d898e284f7
arun <arun@arun-P50IJ.(none)>
2013-06-16 21:58:12 -0400

    Bug fix for return value

--- linked_lists/reverse_linked_list_space_optimal.c
+++ linked_lists/reverse_linked_list_space_optimal.c
@@ -5,9 +5,9 @@ linked_list * reverse2(linked_list * node){
 		int value = node->data;
 		node = remove_node(node, value);
 		linked_list * new_node = create_node(value);
-		new_node->next = new_head;
-		new_head = new_node;
+		new_head = insert_at_beginning(new_head, value);
 	}
+	return new_head;
 }
 int main(){
 	int arr[] = {10,20,30,40,50,60,70};
